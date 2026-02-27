31620f7ec8e665db229d511eac480c489c1b2168
María Victoria <maria.arteagare@campusucc.edu.co>
2026-02-27 15:10:46 -0500

    TALLER ABSTRACT FACTORY, JUEGO DE ROLES

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,17 @@
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
+        GameFactory factoryMedieval;
+        GameFactory factoryFuturistic;
+
+        factoryMedieval = new MedievalFactory();
+        factoryFuturistic = new FuturisticFactory();
+
+        Game gameFuturistic = new Game(factoryFuturistic);
+        gameFuturistic.play();
+
+        Game gameMedieval = new Game(factoryMedieval);
+        gameMedieval.play();
 
     }
 }
